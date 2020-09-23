27cbb9f2fdb22131512d7d9f71506546047c71ca
kuro <kisuvitinh@gmail.com>
2020-09-23 13:30:11 +0700

    views #2

--- views/index.sql
+++ views/index.sql
@@ -48,6 +48,50 @@ CASCADE;
 select * from film_master
 
 
+-- -- -- -- -- -- 3
+
+CREATE VIEW usa_cities AS SELECT
+	city,
+	country_id
+FROM
+	city
+WHERE
+	country_id = 103;
+
+-- 
+SELECT
+	*
+FROM
+	usa_cities;
+
+
+INSERT INTO usa_cities (city, country_id)
+VALUES('San Jose', 103);
+
+
+SELECT
+	city,
+	country_id
+FROM
+	city
+WHERE
+	country_id = 103 and city ilike 'San Jose'
+ORDER BY
+	last_update DESC;
+
+DELETE
+FROM
+	usa_cities
+WHERE
+	city = 'San Jose';
+
+
+
+select * from usa_cities where city ilike 'San Jose'
+
+
+
+
 
 
 
